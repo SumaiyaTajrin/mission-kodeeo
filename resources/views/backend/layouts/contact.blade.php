7ae94149e72284223fb7931584c053f384fa6cc4
SumaiyaTajrin <tamanna17103181@gmail.com>
2021-07-22 19:13:59 +0600

    template

---
+++ resources/views/backend/layouts/contact.blade.php
@@ -0,0 +1,5 @@
+@extends('backend.master')
+
+@section('content')
+<h1> contact us</h1>
+@endsection
---
+++ resources/views/backend/layouts/home.blade.php
@@ -0,0 +1,5 @@
+@extends('backend.master')
+
+@section('content')
+<h1>my home</h1>
+@endsection
